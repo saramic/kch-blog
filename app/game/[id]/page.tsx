9d3aa83d5c0242e32a958b96e20927b1406aa33d
Michael Milewski <saramic@gmail.com>
2024-11-27 08:25:07 +1100

    Switch to google maps as geo: fired off Google Earth
    and the Google Earth link seemed not to go to the lat/long
    
    also generated the shrot codes with
        just generate-qrs \
            https://kch.app/aHI https://kch.app/Bdw https://kch.app/UhQ \
            https://kch.app/v4g https://kch.app/gaw https://kch.app/P6Y \
            https://kch.app/iFo https://kch.app/es4 https://kch.app/f9U \
            https://kch.app/TNo

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -8,34 +8,34 @@ export default async function Page({
     "0DD92EC6-1B34-469D-897B-87106FB7D790": "This is the answer for xyz",
     "31558C7E-9110-4F0E-B54A-00A8D7515919": "This is the answer for abc",
     "0B9396ED-786E-40B0-85D5-3F52D92B6EB7": <>
-      <div>lat,long:</div><a href="geo:-37.938660, 145.235040">-37.93866, 145.23504</a>
+      <div>lat,long:</div><a href="https://google.com/maps/search/-37.938660, 145.235040">-37.93866, 145.23504</a>
     </>, // harcówka carpark
     "5792B0FB-A02F-4E79-9B47-9DA534681252": <>
-      <div>lat,long:</div><a href="geo:-37.937622, 145.231536">-37.937622, 145.231536</a>
+      <div>lat,long:</div><a href="https://google.com/maps/search/-37.937622, 145.231536">-37.937622, 145.231536</a>
     </>, // front gates
     "B0F1CCC2-F100-499D-9F64-3DAA54C526FB": <>
-      <div>lat,long:</div><a href="geo:-37.929900, 145.233609">-37.929900, 145.233609</a>
+      <div>lat,long:</div><a href="https://google.com/maps/search/-37.929900, 145.233609">-37.929900, 145.233609</a>
     </>, // charity bins, near Bergins rd
     "2EC71D74-5D5B-49DD-8FE2-35C5D3D2CCBE": <>
-      <div>lat,long:</div><a href="geo:-37.941633, 145.230683">-37.941633, 145.230683</a>
+      <div>lat,long:</div><a href="https://google.com/maps/search/-37.941633, 145.230683">-37.941633, 145.230683</a>
     </>, // corner stud rd and police rd
     "38F37790-9006-4EE3-8932-92267A2A4A9C": <>
-      <div>lat,long:</div><a href="geo:-37.942147, 145.235540">-37.942147, 145.235540</a>
+      <div>lat,long:</div><a href="https://google.com/maps/search/-37.942147, 145.235540">-37.942147, 145.235540</a>
     </>, // start rough police road
     "82F29E45-1E70-4FE1-AD8E-5A6849340019": <>
-      <div>lat,long:</div><a href="geo:-37.951545, 145.237351">-37.951545, 145.237351</a>
+      <div>lat,long:</div><a href="https://google.com/maps/search/-37.951545, 145.237351">-37.951545, 145.237351</a>
     </>, // historical marker
     "D2918761-2ED6-4250-9A49-753AF9688E73": <>
-      <div>lat,long:</div><a href="geo:-37.956188, 145.240308">-37.956188, 145.240308</a>
+      <div>lat,long:</div><a href="https://google.com/maps/search/-37.956188, 145.240308">-37.956188, 145.240308</a>
     </>, // historical marker
     "69536812-B4BE-4264-9E9C-5E681D021814": <>
-      <div>lat,long:</div><a href="geo:-37.943720, 145.244219">-37.943720, 145.244219</a>
+      <div>lat,long:</div><a href="https://google.com/maps/search/-37.943720, 145.244219">-37.943720, 145.244219</a>
     </>, // start north boundary track
     "69C8E074-24D7-424E-9E09-A77D6DC3A9D3": <>
-      <div>lat,long:</div><a href="geo:-37.944342, 145.254097">-37.944342, 145.254097</a>
+      <div>lat,long:</div><a href="https://google.com/maps/search/-37.944342, 145.254097">-37.944342, 145.254097</a>
     </>, // east stump
     "A824C319-F53D-4AE8-9C04-2D39D4E7FA3A": <>
-      <div>lat,long:</div><a href="geo:-37.944336, 145.253988">-37.944336, 145.253988</a>
+      <div>lat,long:</div><a href="https://google.com/maps/search/-37.944336, 145.253988">-37.944336, 145.253988</a>
     </>, // west stump
   };
   return (
